7e0b197dcfc4b3ddde241810ceab748efc5561c3
adumbidiot <nathaniel.daniel23@outlook.com>
2023-01-04 00:40:58 -0800

    Fix clippy

--- lib/borderlands-launcher/src/bin/generate-protobuf-files.rs
+++ lib/borderlands-launcher/src/bin/generate-protobuf-files.rs
@@ -3,10 +3,10 @@ use std::path::Path;
 fn main() -> anyhow::Result<()> {
     let out_dir = Path::new("src/generated");
 
-    std::fs::create_dir_all(&out_dir)?;
+    std::fs::create_dir_all(out_dir)?;
 
     prost_build::Config::new()
-        .out_dir(&out_dir)
+        .out_dir(out_dir)
         .compile_protos(&["proto/launcher.proto"], &["proto"])?;
 
     Ok(())
